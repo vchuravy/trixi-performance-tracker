1ace1d535c73b9f816d1099ce5ed8e74e910a774
Valentin Churavy <v.churavy@gmail.com>
2025-05-13 15:14:53 +0200

    add rest of benchmarks

--- benchmarks.jl
+++ benchmarks.jl
@@ -9,6 +9,35 @@ for elixir in [
     joinpath(examples_dir(), "tree_1d_dgsem", "elixir_mhd_ec.jl"), 
     joinpath(examples_dir(), "tree_1d_dgsem", "elixir_navierstokes_convergence_walls_amr.jl"),
     joinpath(examples_dir(), "tree_1d_dgsem", "elixir_shallowwater_well_balanced_nonperiodic.jl"),
+    # 2D
+    joinpath(examples_dir(), "tree_2d_dgsem", "elixir_advection_extended.jl"),
+    joinpath(examples_dir(), "tree_2d_dgsem", "elixir_advection_amr_nonperiodic.jl"),
+    joinpath(examples_dir(), "tree_2d_dgsem", "elixir_euler_ec.jl"),
+    joinpath(examples_dir(), "tree_2d_dgsem", "elixir_euler_vortex_mortar.jl"),
+    joinpath(examples_dir(), "tree_2d_dgsem", "elixir_euler_vortex_mortar_shockcapturing.jl"),
+    joinpath(examples_dir(), "tree_2d_dgsem", "elixir_mhd_ec.jl"),
+    joinpath(examples_dir(), "structured_2d_dgsem", "elixir_advection_extended.jl"),
+    joinpath(examples_dir(), "structured_2d_dgsem", "elixir_advection_nonperiodic.jl"),
+    joinpath(examples_dir(), "structured_2d_dgsem", "elixir_euler_ec.jl"),
+    joinpath(examples_dir(), "structured_2d_dgsem", "elixir_euler_source_terms_nonperiodic.jl"),
+    joinpath(examples_dir(), "structured_2d_dgsem", "elixir_mhd_ec.jl"),
+    joinpath(examples_dir(), "unstructured_2d_dgsem", "elixir_euler_wall_bc.jl"), # this is the only elixir working for polydeg=3
+    joinpath(examples_dir(), "p4est_2d_dgsem", "elixir_advection_extended.jl"),
+    joinpath(@__DIR__, "elixir_2d_euler_vortex_tree.jl"),
+    joinpath(@__DIR__, "elixir_2d_euler_vortex_structured.jl"),
+    joinpath(@__DIR__, "elixir_2d_euler_vortex_unstructured.jl"),
+    joinpath(@__DIR__, "elixir_2d_euler_vortex_p4est.jl"),
+    # 3D
+    joinpath(examples_dir(), "tree_3d_dgsem", "elixir_advection_extended.jl"),
+    joinpath(examples_dir(), "tree_3d_dgsem", "elixir_euler_ec.jl"),
+    joinpath(examples_dir(), "tree_3d_dgsem", "elixir_euler_mortar.jl"),
+    joinpath(examples_dir(), "tree_3d_dgsem", "elixir_euler_shockcapturing.jl"),
+    joinpath(examples_dir(), "tree_3d_dgsem", "elixir_mhd_ec.jl"),
+    joinpath(examples_dir(), "structured_3d_dgsem", "elixir_advection_nonperiodic_curved.jl"),
+    joinpath(examples_dir(), "structured_3d_dgsem", "elixir_euler_ec.jl"),
+    joinpath(examples_dir(), "structured_3d_dgsem", "elixir_euler_source_terms_nonperiodic_curved.jl"),
+    joinpath(examples_dir(), "structured_3d_dgsem", "elixir_mhd_ec.jl"),
+    joinpath(examples_dir(), "p4est_3d_dgsem", "elixir_advection_basic.jl"),
     ]
     benchname = basename(dirname(elixir)) * "/" * basename(elixir)
     SUITE[benchname] = BenchmarkGroup()
